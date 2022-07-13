dbb0c681d1c9ec1faf33cb07a62c083ca6a985cd
Neel <madhavneel297@gmail.com>
2022-07-14 05:28:12 +0530

    service.js lower-container 3 carousel setup with 3 image background and some changes made next we have to add body content to carousel

--- src/Sections/Services/Services.js
+++ src/Sections/Services/Services.js
@@ -36,9 +36,41 @@ function Services() {
           <img style={{opacity:"75%"}} src={sidebarimg} alt="" srcset="" />
          </div>
          </div>
+         <div className="mid-span">
+          <span></span>
+         </div>
 
   <div className="lower-container">
+       <div className="axios-data-carousel">
+       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active carousel-one" data-bs-interval="5000">
+       
+       {/* put your content here */}
+       
+    </div>
+    <div class="carousel-item carousel-two" data-bs-interval="5000">
+    
+     {/* put your content here */}
 
+    </div>
+    <div class="carousel-item carousel-three" data-bs-interval="5000">
+    
+     {/* put your content here */}
+
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+       </div>
   </div>
 </div>
   </>;
