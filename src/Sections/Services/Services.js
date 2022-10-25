a49609a1a34345897013da67f745a1822861fae3
neel130 <93971140+neel130@users.noreply.github.com>
2022-10-25 12:50:11 +0530

    changing some layout and design of service section service.js & service.css

--- src/Sections/Services/Services.js
+++ src/Sections/Services/Services.js
@@ -4,28 +4,36 @@ import aIbIimage from "../../assets/service-image/ai-bi-cover-image.jpg"
 import sidebarimg from "../../assets/service-image/ai-bi-sidebar-img.png"
 import datagraph from "../../assets/service-image/data-pie-chart.png"
 import iotIcon  from '../../assets/service-image/iot.svg'
+import ainlp from '../../assets/service-image/ai-nlp.svg'
+import bimodernization from '../../assets/service-image/bi-modernization.png'
+import cloudsolutions from '../../assets/service-image/cloud-solutions.png'
 
 
 function Services() {
   return <>
     <div id='service' className='infoser'  >
       <div className="upper-container">
-        <div className="ai-bi-cover-image">
+        {/* <div className="ai-bi-cover-image">
           <img src={aIbIimage} alt="" srcset="" />
-        </div>
+        </div> */}
 
         <div className="ai-bi-discription">
-          <h1 >BI and AI</h1>
-
-          <h5 style={{ marginBottom: "10px", marginTop: "50px" }} >Nlp Enabled  <svg style={{ height: "20px", width: "50px" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes" viewBox="0 0 16 16">
+          {/* <h1 >BI and AI</h1> */}
+        
+        <div style={{width:"20vw"}} className="nlp-enabled">
+        <img src={ainlp} style={{height:"100px",width:"100px"}} alt="" srcset="" />
+          <h5 style={{ marginBottom: "10px" }} >Nlp Enabled  <svg style={{ height: "20px", width: "50px" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes" viewBox="0 0 16 16">
             <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z" />
           </svg> </h5>
           <p style={{ color: "grey", marginBottom: "40px" }} >NLP enables bots to continuously
             add new synonyms and uses Machine Learning to
             expand chatbot vocabulary while also transfer
             vocabulary from one bot to the next.</p>
-
-
+            <button className='btn btn-success' >Explore More</button>
+            </div>
+        
+        <div style={{width:"25vw"}} className="bi">
+        <img src={bimodernization} style={{height:"100px",width:"100px"}} alt="" srcset="" />
           <h5 style={{ marginBottom: "10px" }} >BI Modernization & Analytics <svg style={{ height: "20px", width: "50px" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
             <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
           </svg> </h5>
@@ -33,11 +41,23 @@ function Services() {
             that are self-service, agile deployments using dev-ops methodologies,
             continuous monitoring and operational efficiencies in your data warehouse deployments
             and cloud based offerings that help you save capital expenditures</p>
+            <button className='btn btn-success' >Explore More</button>
+            </div>
+
+            <div style={{width:"25vw"}} className="cloud solutions">
+        <img src={cloudsolutions} style={{height:"100px",width:"100px"}} alt="" srcset="" />
+          <h5 style={{ marginBottom: "10px" }} >Cloud Solutions  </h5>
+          <p style={{ color: "grey" }} >Cloud computing is the on-demand availability of computer system resources,
+           especially data storage and computing power, without direct active management by the user.
+            Large clouds often have functions distributed over multiple locations,
+             each location being a data center</p>
+            <button className='btn btn-success' >Explore More</button>
+            </div>
         </div>
 
-        <div className="sidebar-box">
+        {/* <div className="sidebar-box">
           <img style={{ opacity: "75%" }} src={sidebarimg} alt="" srcset="" />
-        </div>
+        </div> */}
       </div>
       <div className="mid-span">
         <span></span>
